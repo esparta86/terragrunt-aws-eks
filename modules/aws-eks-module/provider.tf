1ebb4b510eb4174abc99e783547f69867d1c79bc
lisandro_rafaelano <lisandro@nursa.com>
2024-02-19 10:19:14 -0600

    Update repository with last changes

--- modules/aws-eks-module/provider.tf
+++ modules/aws-eks-module/provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region = "us-east-1"
   profile = "personal"
   assume_role {
-    role_arn = "arn:aws:iam::734237051973:role/RolePowerUserAccess"
+    role_arn = "arn:aws:iam::AWS_ACCOUNT:role/YOUR_ROLE"
   }
